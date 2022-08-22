3c8664a7bc5b0579ef00e58748361109f8654630
Sohang  Chopra <sohang.chopra@mfilterit.com>
2022-08-22 15:29:28 +0530

    Progress 62/69

--- exercises/standard_library_types/iterators4.rs
+++ exercises/standard_library_types/iterators4.rs
@@ -1,8 +1,7 @@
 // iterators4.rs
 
-// I AM NOT DONE
-
 pub fn factorial(num: u64) -> u64 {
+    (2 .. num+1).product()
     // Complete this function to return the factorial of num
     // Do not use:
     // - return
