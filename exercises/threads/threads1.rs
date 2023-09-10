9f97ad90b66896553c0c03e4815dd4cf9ff8bcb3
Sohang  Chopra <sohang.chopra@mfilterit.com>
2023-09-10 11:11:37 +0530

    Threads1.rs done, progress 64/69

--- exercises/threads/threads1.rs
+++ exercises/threads/threads1.rs
@@ -5,9 +5,7 @@
 // of "waiting..." and the program ends without timing out when running,
 // you've got it :)
 
-// I AM NOT DONE
-
-use std::sync::Arc;
+use std::sync::{Arc, RwLock};
 use std::thread;
 use std::time::Duration;
 
@@ -16,15 +14,30 @@ struct JobStatus {
 }
 
 fn main() {
-    let status = Arc::new(JobStatus { jobs_completed: 0 });
+    // Arc (Atomic Reference Count) is needed for shared ownership across threads
+    // RwLock is needed so that we can have multiple readers, 1 writer
+    let status = Arc::new(RwLock::new(JobStatus { jobs_completed: 0 }));
+    
+    // if we didn't clone and used status directly, 
+    // then status would be moved into the thread closure
+    // so main thread would not be able to access status.
     let status_shared = status.clone();
+
     thread::spawn(move || {
+        // get a mutable write lock to status
+        let mut status = status_shared.write().unwrap();
         for _ in 0..10 {
             thread::sleep(Duration::from_millis(250));
-            status_shared.jobs_completed += 1;
+            status.jobs_completed += 1;
         }
     });
-    while status.jobs_completed < 10 {
+
+    // in each iteration, acquire read lock to status
+    // and check its value is < 10
+    // NOTE: we CAN'T do status.read().unwrap() outside loop
+    // because status is being updated in another thread, 
+    // so we need to keep re-checking its value.
+    while status.read().unwrap().jobs_completed < 10 {
         println!("waiting... ");
         thread::sleep(Duration::from_millis(500));
     }
